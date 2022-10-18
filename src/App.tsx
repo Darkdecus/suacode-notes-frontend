0b645b5d93744da0a8f00cab45b9b79d32518bd2
Darkdecus <pkaabudu@gmail.com>
2022-10-18 19:43:14 +0000

    scaffod app structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ function App() {
       <header>
         <h1>Suacode Notes App</h1>
       </header>
-      <main>div</main>
+      <main>
+        <article>List Notes here</article>
+        <article>Conditional render slected note here</article>
+      </main>
       <footer>
         <p>Made by Patrick with ❤ &copy;2022</p>
       </footer>
