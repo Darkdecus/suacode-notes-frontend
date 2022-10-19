c3033045f57ce76d9d4c7bdf7116f69f93a22d16
Darkdecus <pkaabudu@gmail.com>
2022-10-19 04:11:14 +0000

    using prop value to render notes

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,13 +1,15 @@
 import { Dispatch, SetStateAction } from "react";
+import { INote } from "../dtos/Note.dto";
 import ListItem from "./ListItem";
 import { ListWrapper, StyledList } from "./NoteList";
 
 interface IProps {
   selectedNote: string | number | null;
   setSelectedNote: Dispatch<SetStateAction<string | number | null>>;
+  notes: INote[];
 }
 
-function NoteList({ selectedNote, setSelectedNote }: IProps) {
+function NoteList({ selectedNote, setSelectedNote, notes }: IProps) {
   // const handleClick = (id: string | number) => {
   //   setSelectedNote(id);
   //   return (e: any) => {
@@ -19,69 +21,9 @@ function NoteList({ selectedNote, setSelectedNote }: IProps) {
     <ListWrapper noteId={selectedNote}>
       <h2>Notes</h2>
       <StyledList>
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
-        <ListItem
-          handler={setSelectedNote}
-          note={{
-            _id: Math.round(Math.random() * 1e6),
-            title: "The title of the note",
-            createdAt: new Date(Date.now()),
-            updatedAt: new Date(Date.now()),
-          }}
-        />
+        {notes.map((note) => (
+          <ListItem key={note._id} handler={setSelectedNote} note={note} />
+        ))}
       </StyledList>
     </ListWrapper>
   );
