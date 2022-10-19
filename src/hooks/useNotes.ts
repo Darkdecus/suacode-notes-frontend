7e06c679e6ac7dee6767d0d12c881a98a261ae81
Darkdecus <pkaabudu@gmail.com>
2022-10-19 01:21:18 +0000

    added return type to useNotes

--- src/hooks/useNotes.ts
+++ src/hooks/useNotes.ts
@@ -1,6 +1,7 @@
+import { INote } from "../components/dtos/Note.dto";
 import { NOTES } from "../dummy-data";
 
-export const useNotes = () => {
+export const useNotes = (): { notes: INote[] } => {
   return {
     notes: NOTES,
   };
