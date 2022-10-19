37987a507dc0c283228ba7ffb2551797a94da15f
Darkdecus <pkaabudu@gmail.com>
2022-10-19 01:18:12 +0000

    dummy data

---
+++ src/dummy-data.ts
@@ -0,0 +1,57 @@
+export const NOTES = [
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+  {
+    _id: Math.round(Math.random() * 1e6),
+    title: "The title of the note" + Math.round(Math.random() * 1e3),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+  },
+];
